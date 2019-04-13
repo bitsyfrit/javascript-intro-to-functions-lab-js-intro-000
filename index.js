30c256f74d80d9ca4a28807a5dbeac1b83be0fd7
Elizabeth Fritts <bethfritts@yahoo.com>
2019-04-13 17:01:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,6 @@ function sayHiToGrandma(string) {
   return("I can\'t hear you!")
   else if(string.toUpperCase() === string)
   return("YES INDEED!")
+  else if("I love you, Grandma.")
+  return("I love you, too.")
 }
