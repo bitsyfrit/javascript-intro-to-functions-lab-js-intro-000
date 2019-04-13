61db3b488c07844894c0ae3130bc9d1fd8c505aa
Elizabeth Fritts <bethfritts@yahoo.com>
2019-04-13 16:40:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  if (string.toUpperCase() === string.uppercase)
+  if (string.toUpperCase() === string)
   console.log(string)
   else
   return string.toUpperCase()
